5b293f0b4d3d5efb4818500dd1458d5d15c58915
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-09 02:59:55 +0800

    Did the following:
    1. Configured App component.
    2. Created HomePage.
    3. Created LoginPage.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 const App = () => {
   return (
-    <div>Hello world, and so on and so forth...</div>
+    <div>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
+    </div>
   )
 }
 
