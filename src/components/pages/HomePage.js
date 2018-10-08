411e1b7d5207688db1ad9de81a197e9d7b17e241
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-09 04:28:20 +0800

    Installed the following:
    1. semantic-ui-css
    2. semantic-ui-react

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>
-            <Link to="/login">LOgin</Link>
+            <Link to="/login">Login</Link>
         </div>    
     )
 };
