70d8c09282dfb5e806a93f1de6270d05cb109727
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-09 14:03:44 +0800

    Did the following:
    1. Created LoginForm
    2. In LoginPage, imported LoginForm

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
+class LoginForm extends Component {
+    state = {
+        data: {},
+        loading: false,
+        errors: {}
+    };
+
+    render() {
+        return (
+            <Form>
+                <Button primary>Login</Button>
+            </Form>
+        );
+    }
+}
+
+export default LoginForm;
