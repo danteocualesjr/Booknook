ad6d6d40d5e2b747bb110c0edf45a4fc6885cd62
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-10-10 02:57:49 +0800

    Did the following:
    1. Installed validator.
    2. Added email and password forms in LoginForm.

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -3,14 +3,57 @@ import { Form, Button } from 'semantic-ui-react';
 
 class LoginForm extends Component {
     state = {
-        data: {},
+        data: {
+            email: '',
+            password: ''
+        },
         loading: false,
         errors: {}
     };
 
+    onChange = e => 
+        this.setState({ 
+            data: { ...this.state.data, [e.target.name]: e.target.value } 
+        });
+
+    onSubmit = () => {
+        const errors = this.validate(this.state.data);
+        this.setState({ errors });
+    };
+
+    validate = (data) => {
+        const errors = {};
+        if (!data.password) errors.password = "Can't be blank";
+        return errors;
+    }
+
     render() {
+        const { data } = this.state;
+
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        placeholder="example@example.com" 
+                        value={data.email}
+                        onChange={this.onChange}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="At least 8 characters" 
+                        value={data.password}
+                        onChange={this.onChange}
+                    />
+                </Form.Field>
                 <Button primary>Login</Button>
             </Form>
         );
